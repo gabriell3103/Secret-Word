861f064418baaca91bc7408434c3f7526a45c75a
gabriell3103 <g5819853@gmail.com>
2024-08-29 15:10:45 -0300

    Inserindo o fluxo de telas por componente

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -21,12 +21,27 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
+
+  // Starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
   
+  // process the letter inputs
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className='App'>
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "start" && <Game />}
-      {gameStage === "start" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   )
 }
