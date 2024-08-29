1aecd240d77f1f9cae1881bdf49950ea8704a663
gabriell3103 <g5819853@gmail.com>
2024-08-29 15:02:27 -0300

    Criação dos componentes

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -1,11 +1,32 @@
+// CSS
 import './App.css'
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import {wordsList} from "./data/words"
+
+// Components
 import StartScreen from './components/StartScreen';
+import GameOver from './components/GameOver';
+import Game from './components/Game';
+  
+const stages = [
+      {id: 1, name: "start"},
+      {id: 2, name: "game"},
+      {id: 3, name: "end" }
+    ]
 
 function App() {
-
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "start" && <Game />}
+      {gameStage === "start" && <GameOver />}
     </div>
   )
 }
