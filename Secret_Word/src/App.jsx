667e334cabdc0b478d91ea06e1cdc01e407479a7
gabriell3103 <g5819853@gmail.com>
2024-08-30 01:41:18 -0300

    Implementando a lógica

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -18,6 +18,8 @@ const stages = [
       {id: 3, name: "end" }
     ]
 
+const guessesQty = 3
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
@@ -63,12 +65,52 @@ function App() {
   }
   
   // process the letter inputs
-  const verifyLetter = () => {
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase()
+
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter))
+      {
+      return
+    }
+
+    // push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ])
+
+      setGuesses((actualGuesses) => actualGuesses - 1)
+    }
   }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
+  useEffect(() => {
+    if(guesses <= 0){
+      // reset all states
+      clearLetterStates()
+
+      setGameStage(stages[2].name)
+    }
+  }, [guesses])
 
   // restarts the game
   const retry = () => {
+    setScore(0)
+    setGuesses(guessesQty)
+    
     setGameStage(stages[0].name)
   }
 
--- Secret_Word/src/components/Game.jsx
+++ Secret_Word/src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import "./Game.css"
 
 const Game = ({
@@ -10,6 +11,20 @@ const Game = ({
     guesses, 
     score
 }) => {
+  
+  const [letter, setLetter] = useState("")
+  const letterRefInput = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    verifyLetter(letter)
+
+    setLetter("")
+
+    letterRefInput.current.focus()  
+  }
+
   return (
     <div className="game">
         <p className="points">
@@ -33,15 +48,23 @@ const Game = ({
         </div>
         <div className="letterContainer">
             <p>Tente adivinhar uma letra da palavra:</p>
-            <form>
-                <input type="text" name="letter" maxLength="1" required/>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                name="letter" 
+                maxLength="1" 
+                required 
+                onChange={(e) => setLetter(e.target.value)} 
+                value={letter}
+                ref={letterRefInput}
+                />
                 <button>Jogar!</button>
             </form>
         </div>
         <div className="wrongLetterContainer">
             <p>Letras ja utilizadas:</p>
             {wrongLetters.map((letter, i) => (
-                <span key={i}>{letter}</span>
+                <span key={i}>{letter},</span>
             ))}
         </div>
     </div>
