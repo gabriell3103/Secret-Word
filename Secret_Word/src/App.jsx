687e914610937320935d58554b36b814c930ab59
gabriell3103 <g5819853@gmail.com>
2024-08-30 01:46:43 -0300

    Css do fim de jogo

--- Secret_Word/src/App.jsx
+++ Secret_Word/src/App.jsx
@@ -129,7 +129,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry}/>}
+      {gameStage === "end" && <GameOver retry={retry} score={score}/>}
     </div>
   )
 }
